54d058bd31e7eba2c0e6faf85f037b10d4aa18ea
Matias Labra <gmlabra@gmail.com>
2020-03-26 12:32:43 +1000

    feat: script to build and deploy in github master branch

--- deploy.sh
+++ deploy.sh
@@ -1,5 +1,5 @@
 rm -rf build  && yarn build && \                                              
 cd build  && git init && git add . \
-&& git commit -m "Initial commit" && \
+&& git commit -m "Last build from react branch" && \
 git remote add origin https://github.com/matiaslabra/matiaslabra.github.io.git && \
 git push --force origin master && cd ..
