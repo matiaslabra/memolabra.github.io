78dcfee57051f9042812c7eafed8502f4264e37b
Matias Labra <gmlabra@gmail.com>
2020-04-21 20:31:47 +1000

    Last build from react branch

---
+++ precache-manifest.51eca50e70e64641db3f8332207ecc65.js
@@ -0,0 +1,46 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "4ad072e134dfd5eba6c0327ac9ff97e5",
+    "url": "/index.html"
+  },
+  {
+    "revision": "3e996089175eaca69515",
+    "url": "/static/css/main.18dc7b1a.chunk.css"
+  },
+  {
+    "revision": "039da0cdfafe0ec24e55",
+    "url": "/static/js/2.85d76a16.chunk.js"
+  },
+  {
+    "revision": "51255ce84f566e10a705c5ca98edd89d",
+    "url": "/static/js/2.85d76a16.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "b039fb710a8f15c87d3a",
+    "url": "/static/js/3.b69f6ec0.chunk.js"
+  },
+  {
+    "revision": "0f2be52aac348a57b5e1722f475cbfd0",
+    "url": "/static/js/3.b69f6ec0.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "91123e41f30271d4a23f",
+    "url": "/static/js/4.92190cc3.chunk.js"
+  },
+  {
+    "revision": "3e996089175eaca69515",
+    "url": "/static/js/main.334c4dfe.chunk.js"
+  },
+  {
+    "revision": "38aac50cdf5e1088c459",
+    "url": "/static/js/runtime-main.c1321474.js"
+  },
+  {
+    "revision": "8a9b10612db2f2499faae04d8c2bcb1a",
+    "url": "/static/media/site1x1200.8a9b1061.png"
+  },
+  {
+    "revision": "daa4c0490aab3a72606479f7929cae61",
+    "url": "/static/media/site2x1200.daa4c049.png"
+  }
+]);
